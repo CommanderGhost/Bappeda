1d49acddf8e305bc12bc4010ff5818582370e4e2
CommanderGhost <agishatake@gmail.com>
2020-02-07 00:25:10 +0700

    Data Umum 1/2 Jadi

---
+++ data-umum.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+ <!-- Import CDN Boostrap CSS -->
+ <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+ <!-- Import CDN Boostrap CSS -->   
+    <title>Ganti Password</title>
+</head>
+<body>
+
+<!-- Navbar -->
+<div class="card">
+  <div class="card-header bg-success">
+  <div class="d-flex flex-row bd-highlight">
+  <div class="bd-highlight"><img src="img/logo.png" class="img-fluid center" alt="Logo Bantul" width="60px"></div>
+  <div class="bd-highlight ml-3 text-white">Aplikasi Sentralisasi Data
+  <span class="float2"><a href="#" role="button" class="btn" style="background-color:#b5e61d">Login</a></span>
+  <span class="float3"><a href="#" role="button" class="btn" style="background-color:#b5e61d">Beranda</a></span>
+  <div style="color:black"><h5>BADAN PERENCANAAN PEMBANGUNAN DAERAH (BAPPEDA)</h5></div>
+  <div style="color:black"><h5>KABUPATEN BANTUL</h5></div>
+  </div>
+</div>
+  </div>
+</div>
+<!-- Navbar -->
+
+<!-- Navbar 2 -->
+<nav class="navbar navbar-expand-lg" style="background-color:#b5e61d">
+  <div class="collapse navbar-collapse justify-content-center" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link" href="#" style="color:black">Data Umum</a>
+      <a class="nav-item nav-link" href="#" style="color:black">Sosial/Budaya</a>
+      <a class="nav-item nav-link" href="#" style="color:black">SDA</a>
+      <a class="nav-item nav-link" href="#" style="color:black">Infrasturktur</a>
+      <a class="nav-item nav-link" href="#" style="color:black">Ekonomi</a>
+      <a class="nav-item nav-link" href="#" style="color:black">Polhumkam</a>
+      <a class="nav-item nav-link" href="#" style="color:black">Idensial</a>
+    </div>
+  </div>
+</nav>
+<!-- Navbar 2 -->
+
+
+<div class="float">
+        <img src="img/bantul2.png" alt="Brand Bantul" width="900px">
+    </div>
+
+
+    <!-- Import CDN  Boostrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script> 
+    <!-- Import CDN  Boostrap JS -->
+
+</body>
+</html>
+
+<style type="text/css">
+
+body { 
+ background: url(img/back.jpg) no-repeat center center fixed; 
+ -webkit-background-size: cover;
+ -moz-background-size: cover;
+ -o-background-size: cover;
+ background-size: cover;
+}
+
+.center {
+display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.float{
+	position:fixed;
+	width:10px;
+	height:60px;
+	bottom:8px;
+	left:10px;
+	text-align:center;
+}
+
+.float2{
+	position:fixed;
+	width:100px;
+	height:60px;
+	top:8px;
+	right:10px;
+	text-align:center;
+}
+
+.float3{
+	position:fixed;
+	width:100px;
+	height:60px;
+	top:8px;
+	right:100px;
+	text-align:center;
+}
+
+.navbar {
+  height: 35px;
+}
+
+a:hover {
+  background-color: yellow;
+</style>
+
+<script>
+
+</script>
