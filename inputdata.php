178394288815ef974d2b410cec3b3d964f1a4a78
CommanderGhost <agishatake@gmail.com>
2020-04-07 19:00:47 +0700

    input data

--- inputdata.php
+++ inputdata.php
@@ -6,38 +6,10 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
  <!-- Import CDN Boostrap CSS -->
- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+ <link href="fa\css\all.css" rel="stylesheet">
+ <link rel="stylesheet" href="bs\css\bootstrap.min.css">
  <!-- Import CDN Boostrap CSS -->   
-    <title>Master Data</title>
-    <link rel="stylesheet" type="text/css" href="css/form.css">
-    <style type="text/css">
-    .ul_menu {
-          list-style-type: none;
-          margin: 0;
-          padding-left: 40px;
-          padding-right: 40px;
-          overflow: hidden;
-          background-color: #7CFC00;
-        }
-
-        .li_menu {
-            font-size: 15px;
-            float: left;
-        }
-
-        .li_menu a {
-          display: block;
-          color: black;
-          text-align: center;
-          padding: 14px 16px;
-          text-decoration: none;
-        }
-
-        .li_menu a:hover {
-          background-color: yellow;
-        }
-        
-        </style>
+    <title>Input Data</title>
 </head>
 <body>
 
@@ -47,48 +19,136 @@
   <div class="d-flex flex-row bd-highlight">
   <div class="bd-highlight"><img src="img/logo.png" class="img-fluid center" alt="Logo Bantul" width="60px"></div>
   <div class="bd-highlight ml-3 text-white">Aplikasi Sentralisasi Data
+  <span class="float2">
+   <!-- Notifikasi -->
+   <div class="dropdown dropleft">
+  <button class="btn bg-warning" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    <i class="fas fa-bell"></i>
+  </button>
+  <ul class="dropdown-menu drop2">
+      <li class="notification-box">
+          <div class="row">
+            <div class="col">
+            <div>
+            <p>Lorem ipsum dolor sit amet, consectetur</p>
+              </div>
+             <small class="text-warning">27.11.2015, 15:00</small>
+            </div>
+          </div>
+      </li>
+      <li class="notification-box">
+          <div class="row">
+            <div class="col">
+            <div>
+            <p>Lorem ipsum dolor sit amet, consectetur</p>
+              </div>
+             <small class="text-warning">27.11.2015, 15:00</small>
+            </div>
+          </div>
+      </li>
+  </ul>
+</div>
+   <!-- Notifikasi -->
+  </span>
   <div style="color:black"><h5>BADAN PERENCANAAN PEMBANGUNAN DAERAH (BAPPEDA)</h5></div>
   <div style="color:black"><h5>KABUPATEN BANTUL</h5></div>
   </div>
-  
 </div>
   </div>
 </div>
 <!-- Navbar -->
 
-<div>
-            <ul class="ul_menu">
-              <li class="li_menu"><a href="#">Input Data</a></li>
-              <li class="li_menu"><a href="#">Keluar</a></li>
-            </ul>
-</div>
+<!-- Navbar 2 -->
+<nav class="navbar navbar-expand-lg" style="background-color:#b5e61d">
+  
+  <div class="collapse navbar-collapse justify-content-center" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="#" style="color:black">Beranda</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" style="color:black" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Pengguna
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#">Ubah Password</a>
+          <a class="dropdown-item" href="#">Ubah Profil</a>
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" style="color:black" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Administrator
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#">Sumber Data</a>
+          <a class="dropdown-item" href="#">Elemen Data</a>
+          <a class="dropdown-item" href="#">Justifikasi Elemen Data</a>
+          <a class="dropdown-item" href="#">Daftar User OPD</a>
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" href="#" style="color:black" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Manajemen Data
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#">Pencarian Data</a>
+          <a class="dropdown-item" href="#">Download Template</a>
+          <a class="dropdown-item" href="#">Input Data</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#" style="color:black">Master Data</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#" style="color:black">Laporan</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#" style="color:black">Monitoring</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#" style="color:black">Usulan Elemen Data</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link mx-4" href="#" style="color:black">Keluar</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+<!-- Navbar 2 -->
 
+<!-- Isi Data -->
 <div>
-            <table border="0" width="100%" align="center">
-                <tr>
-                    <td width="25%" bgcolor="yellow" height="70px" align="center">
-                    <div class="dropdown">
+    <table border="0" width="100%" align="center">
+      <tr>
+        <td width="25%" bgcolor="yellow" height="70px" align="center">
+        <div class="dropdown">
   <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
     Pencarian Data
   </button>
-  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-    <a class="dropdown-item" href="#">Action</a>
-    <a class="dropdown-item" href="#">Another action</a>
-    <a class="dropdown-item" href="#">Something else here</a>
-  </div>
+    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+      <a class="dropdown-item" href="#">Action</a>
+      <a class="dropdown-item" href="#">Another action</a>
+      <a class="dropdown-item" href="#">Something else here</a>
+    </div>
   <button type="button" class="btn btn-success">Pilih</button>
 </div><br>
-                    </td>
-                    <td rowspan="7" width="30px"></td>
-                    <td rowspan="7" valign="top">
-                        <h3>Kondisi Pendidikan Anak Usia Dini</h3><br>
-                        <table border="1">
-                        <button type="button" class="btn btn-success">Print</button>
-                         <button type="button" class="btn btn-success">Download</button>
-                         <div class="dropdown">
-  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    2019
-  </button>
+      </td>
+         <td rowspan="7" width="30px"></td>
+             <td rowspan="7" valign="top">
+                <h5 class="text-center">Kondisi Pendidikan Anak Usia Dini</h5><br>
+                 <table border="1">
+                 <div class="mb-2">
+                      <div class="dropdown">
+      <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        2019
+      </button>
+        <button type="button" class="btn btn-success">Print</button>
+        <button type="button" class="btn btn-success">Download</button>
+        <button type="button" class="btn btn-danger">Clear Data</button>
+        <button type="button" class="btn btn-warning">Copy Data</button>
+        <button type="button" class="btn btn-primary">Proses</button>
+        <button type="button" class="btn btn-success">Grafik</button>
+        </div>
   <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
     <a class="dropdown-item" href="#">Action</a>
     <a class="dropdown-item" href="#">Another action</a>
@@ -210,11 +270,79 @@
                 </tr>
             </table>
         </div>
+<!-- Isi Data -->
 
-<!-- Import CDN  Boostrap JS -->
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script> 
+
+<div class="float">
+        <img src="img/bantul2.png" alt="Brand Bantul" width="900px">
+    </div>
+
+
+    <!-- Import CDN  Boostrap JS -->
+    <script src="bs\js\popper.min.js"></script>
+    <script src="bs\js\slim.min.js"></script>
+    <script src="bs\js\bootstrap.min.js"></script>
     <!-- Import CDN  Boostrap JS -->
 
-    
+</body>
+</html>
+
+<style type="text/css">
+
+body { 
+ background: url(img/back.jpg) no-repeat center center fixed; 
+ -webkit-background-size: cover;
+ -moz-background-size: cover;
+ -o-background-size: cover;
+ background-size: cover;
+}
+
+.center {
+display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.float{
+	position:fixed;
+    z-index:-100;
+	width:10px;
+	height:60px;
+	bottom:3px;
+	left:10px;
+	text-align:center;
+}
+
+.navbar {
+  height: 35px;
+}
+
+a:hover {
+  background-color: yellow;
+}
+
+.float2{
+	position:absolute;
+	width:100px;
+	height:60px;
+	top:8px;
+	right:100px;
+	text-align:center;
+}
+
+.notification-box{
+  padding: 10px;
+}
+
+.drop2 {
+    width: 300px !important;
+}
+.breadcrumb {
+    padding: 2px 15px !important;
+}
+
+</style>
+
+<script>
+
+</script>
